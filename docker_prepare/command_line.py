ece5bbf87cc8ca83173635cfa5ceb4eec6ea3b42
Javid Mougamadou <javidjms0@gmail.com>
2019-10-26 00:08:57 +0200

    [feat]: Add input option for main() in command_line.py

--- docker_prepare/command_line.py
+++ docker_prepare/command_line.py
@@ -2,6 +2,13 @@ import click
 
 
 @click.group(invoke_without_command=True)
+@click.option(
+    '-i',
+    '--input',
+    'input_docker_template_path',
+    type=click.Path(exists=True),
+    default='Dockertemplate',
+)
 @click.pass_context
 def main(ctx, *args, **kwargs):
     if ctx.invoked_subcommand is None:
