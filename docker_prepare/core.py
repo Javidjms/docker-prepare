d3c3a18f2f11c7ed745edf645528fffafbbca751
Javid Mougamadou <javidjms0@gmail.com>
2019-10-26 00:15:11 +0200

    [feat]: Add write_file staticmethod in Core

--- docker_prepare/core.py
+++ docker_prepare/core.py
@@ -1,2 +1,5 @@
 class Core:
-    pass
+    @staticmethod
+    def write_file(file_path, rendered_content):
+        with open(file_path, "w") as text_file:
+            text_file.write(rendered_content)
