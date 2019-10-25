963930eef8bd6857d943457e369ef1e3addfc534
Javid Mougamadou <javidjms0@gmail.com>
2019-10-26 00:16:02 +0200

    [feat]: Add render_docker_file staticmethod in Core

--- docker_prepare/core.py
+++ docker_prepare/core.py
@@ -1,4 +1,22 @@
+from pathlib import Path
+from jinja2 import Environment, FileSystemLoader
+
+
 class Core:
+    @staticmethod
+    def render_docker_file(file_path, env_vars={}):
+        file_name = Path(file_path).name
+        parent_dir = Path(file_path).parent.absolute().as_posix()
+
+        j2_env = Environment(
+            loader=FileSystemLoader(parent_dir),
+            trim_blocks=True,
+        )
+        rendered_docker_file = j2_env\
+            .get_template(file_name)\
+            .render(**env_vars)
+        return rendered_docker_file
+
     @staticmethod
     def write_file(file_path, rendered_content):
         with open(file_path, "w") as text_file:
